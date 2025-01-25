ac47ed19574eb35c3a5119cdbc1f3ec967f71f0c
bhaveshrode <bhaveshrode2004@gmail.com>
2025-01-25 11:27:19 +0530

    Create web3.js

---
+++ src/web3.js
@@ -0,0 +1,13 @@
+import Web3 from 'web3';
+
+let web3;
+
+if (window.ethereum) {
+    web3 = new Web3(window.ethereum);
+    window.ethereum.enable(); // Request account access
+}
+else {
+    console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+}
+
+export default web3;
